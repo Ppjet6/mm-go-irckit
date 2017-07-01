fdaaa86562a77e1cb5a87f67ef8f4005bedda7ea
Wim <wim@42.be>
2017-07-01 17:40:04 +0200

    Show the original message/author after replied messages (from @recht matterircd fork)

--- mmuser.go
+++ mmuser.go
@@ -1,6 +1,7 @@
 package irckit
 
 import (
+	"fmt"
 	"net"
 	"regexp"
 	"strings"
@@ -195,6 +196,18 @@ func (u *User) handleWsActionPost(rmsg *model.WebSocketEvent) {
 			return
 		}
 	}
+
+	if data.ParentId != "" {
+		parent, err := u.mc.Client.GetPost(data.ChannelId, data.ParentId, "")
+		if err != nil {
+			logger.Debugf("Unable to get parent post for", data)
+		} else {
+			parentPost := parent.Data.(*model.PostList).Posts[data.ParentId]
+			parentGhost := u.createMMUser(u.mc.GetUser(parentPost.UserId))
+			data.Message = fmt.Sprintf("%s (re @%s: %s)", data.Message, parentGhost.Nick, parentPost.Message)
+		}
+	}
+
 	// create new "ghost" user
 	ghost := u.createMMUser(u.mc.GetUser(data.UserId))
 	// our own message, set our IRC self as user, not our mattermost self

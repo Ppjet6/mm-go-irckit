e84860b0036795be5e51cb4d3b62b74e25eed738
Wim <wim@42.be>
2017-07-01 22:23:58 +0200

    Execute commands in goroutine in order to unblock processing. (from @recht matterircd fork)

--- server.go
+++ server.go
@@ -348,14 +348,15 @@ func (s *server) handle(u *User) {
 			// Ignore empty messages
 			continue
 		}
-
-		err = s.commands.Run(s, u, msg)
-		if err == ErrUnknownCommand {
-			// TODO: Emit event?
-		} else if err != nil {
-			logger.Errorf("handler error for %s: %s", u.ID(), err.Error())
-			return
-		}
+		go func() {
+			err = s.commands.Run(s, u, msg)
+			logger.Debug("Executed", msg, err)
+			if err == ErrUnknownCommand {
+				// TODO: Emit event?
+			} else if err != nil {
+				logger.Errorf("handler error for %s: %s", u.ID(), err.Error())
+			}
+		}()
 	}
 }
 
